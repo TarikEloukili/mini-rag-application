9dd0d892221ed3e36fc3bd859ca86cfa64b36c23
TarikEloukili <tarikeloukili03@gmail.com>
2025-06-14 16:13:18 +0100

    finished tuto 7

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
+from routes import base
+
 app = FastAPI()
 
+app.include_router(base.base_router)
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to the FastAPI application!"}
+
+
 
