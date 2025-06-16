275702e1c9bd650e428b7de9ad12362f0a242401
TarikEloukili <tarikeloukili03@gmail.com>
2025-06-16 17:58:19 +0100

    finished tuto 8

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -6,3 +6,5 @@ class ResponseSignal(Enum):
     FILE_SIZE_EXCEEDED = "File size exceeded the maximum allowed size"
     FILE_UPLOAD_SUCCESS = "File uploaded successfully"
     FILE_UPLOAD_FAILED = "File upload failed"
+    FILE_PROCESSING_FAILED = "File processing failed"
+    FILE_PROCESSING_SUCCESS = "File processed successfully"
