402e1c8f3cebf71c10e71e81bccdce68a2bab077
TarikEloukili <tarikeloukili03@gmail.com>
2025-06-26 18:26:31 +0100

    finished tutorial number 10

--- src/controllers/ProjectController.py
+++ src/controllers/ProjectController.py
@@ -1,11 +1,10 @@
 from .BaseController import BaseController
-from fastapi import UploadFile
-from models import ResponseSignal
 import os
 
 class ProjectController(BaseController):
     
     def __init__(self):
+        
         super().__init__()
 
     def get_project_path(self, project_id: str):
